6213ba3ba8bb2dc8f77b34c65f54ef966b03b4b5
Stephan <stephansokolov01@gmail.com>
2023-04-24 14:29:16 -0700

    fixed syntax error.

--- installDependencies.sh
+++ installDependencies.sh
@@ -109,57 +109,59 @@ fi
 # Clean up the temporary file
 rm /tmp/apt_error
 
-if $JETPACK_FOUND; then# Handle Jetson installation:
-    if $TAR_SPECIFIED; then
-        sudo tar -xvf "$TAR_FILE_PATH" -C /
-        cd /opt/nvidia/deepstream/deepstream-6.2
-        sudo ./install.sh
-        sudo ldconfig
-        echo "Finished installation of dependencies."
-        deepstream-app --version
-    else
-        echo "Dependencies installed, now install DeepStream from NVIDIA's website."
-    fi
-else # Handle non-Jetson installation: (dGPU platform)
+if $JETPACK_FOUND; then
+  # Handle Jetson installation:
+  if $TAR_SPECIFIED; then
+    sudo tar -xvf "$TAR_FILE_PATH" -C /
+    cd /opt/nvidia/deepstream/deepstream-6.2
+    sudo ./install.sh
+    sudo ldconfig
+    echo "Finished installation of dependencies."
+    deepstream-app --version
+  else
+    echo "Dependencies installed, now install DeepStream from NVIDIA's website."
+  fi
+else
+  # Handle non-Jetson installation: (dGPU platform)
 
-    # CUDA Toolkit 11.8
-    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
-    sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-    sudo apt-get update
-    sudo apt-get install cuda-toolkit-11-8
+  # CUDA Toolkit 11.8
+  sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+  sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+  sudo apt-get update
+  sudo apt-get install cuda-toolkit-11-8
 
-    # NVIDIA driver 525.85.12
-    if $RUN_SPECIFIED; then
-        # kill services prior to installation of driver
-        sudo service gdm stop 
-        sudo service lightdm stop 
-        sudo pkill -9 Xorg
-        # Set permissions and run NVIDIA installer
-        chmod 755 "$RUN_FILE_PATH"
-        RUN_DIR="$(dirname "$RUN_FILE_PATH")"
-        cd "$RUN_DIR"
-        sudo ./NVIDIA-Linux-x86_64-525.85.12.run --no-cc-version-check
-        # restart killed services
-        sudo service gdm start
-        sudo service lightdm start
-    fi
+  # NVIDIA driver 525.85.12
+  if $RUN_SPECIFIED; then
+    # kill services prior to installation of driver
+    sudo service gdm stop 
+    sudo service lightdm stop 
+    sudo pkill -9 Xorg
+    # Set permissions and run NVIDIA installer
+    chmod 755 "$RUN_FILE_PATH"
+    RUN_DIR="$(dirname "$RUN_FILE_PATH")"
+    cd "$RUN_DIR"
+    sudo ./NVIDIA-Linux-x86_64-525.85.12.run --no-cc-version-check
+    # restart killed services
+    sudo service gdm start
+    sudo service lightdm start
+  fi
 
-    # TensorRT 8.5.2.2
-    sudo apt-get install libnvinfer8=8.5.2-1+cuda11.8 libnvinfer-plugin8=8.5.2-1+cuda11.8 libnvparsers8=8.5.2-1+cuda11.8 \
-    libnvonnxparsers8=8.5.2-1+cuda11.8 libnvinfer-bin=8.5.2-1+cuda11.8 libnvinfer-dev=8.5.2-1+cuda11.8 \
-    libnvinfer-plugin-dev=8.5.2-1+cuda11.8 libnvparsers-dev=8.5.2-1+cuda11.8 libnvonnxparsers-dev=8.5.2-1+cuda11.8 \
-    libnvinfer-samples=8.5.2-1+cuda11.8 libcudnn8=8.7.0.84-1+cuda11.8 libcudnn8-dev=8.7.0.84-1+cuda11.8 \
-    python3-libnvinfer=8.5.2-1+cuda11.8 python3-libnvinfer-dev=8.5.2-1+cuda11.8
+  # TensorRT 8.5.2.2
+  sudo apt-get install libnvinfer8=8.5.2-1+cuda11.8 libnvinfer-plugin8=8.5.2-1+cuda11.8 libnvparsers8=8.5.2-1+cuda11.8 \
+  libnvonnxparsers8=8.5.2-1+cuda11.8 libnvinfer-bin=8.5.2-1+cuda11.8 libnvinfer-dev=8.5.2-1+cuda11.8 \
+  libnvinfer-plugin-dev=8.5.2-1+cuda11.8 libnvparsers-dev=8.5.2-1+cuda11.8 libnvonnxparsers-dev=8.5.2-1+cuda11.8 \
+  libnvinfer-samples=8.5.2-1+cuda11.8 libcudnn8=8.7.0.84-1+cuda11.8 libcudnn8-dev=8.7.0.84-1+cuda11.8 \
+  python3-libnvinfer=8.5.2-1+cuda11.8 python3-libnvinfer-dev=8.5.2-1+cuda11.8
 
-    # Finally, unpack DeepStream tar file.
-    if $TAR_SPECIFIED; then
-        sudo tar -xvf "$TAR_FILE_PATH" -C /
-        cd /opt/nvidia/deepstream/deepstream-6.2
-        sudo ./install.sh
-        sudo ldconfig
-        echo "Finished installation of dependencies."
-        deepstream-app --version
-    else
-        echo "Dependencies installed, now install DeepStream from NVIDIA's website."
-    fi
+  # Finally, unpack DeepStream tar file.
+  if $TAR_SPECIFIED; then
+    sudo tar -xvf "$TAR_FILE_PATH" -C /
+    cd /opt/nvidia/deepstream/deepstream-6.2
+    sudo ./install.sh
+    sudo ldconfig
+    echo "Finished installation of dependencies."
+    deepstream-app --version
+  else
+    echo "Dependencies installed, now install DeepStream from NVIDIA's website."
+  fi
 fi
